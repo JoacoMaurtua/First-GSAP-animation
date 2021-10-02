1ebdaf67a8fbbe9e627cb6b1405b0a4d9076fb16
JoacoMaurtua <joacomaurtua@gmail.com>
2021-10-01 21:14:53 -0500

    styles page complete

--- first-animation/src/components/Page.js
+++ first-animation/src/components/Page.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import RapOne from '../images/rap-1.jpg';
 import RapTwo from '../images/rap-2.jpg';
+import './Style.css';
 
 const Page = () => {
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="Info">
-          <div className="CenterInfo">
-              <h2>Welcome to</h2>
-              <h2>Introduction</h2>
-              <h1>HIPHOP</h1>
-              <h2>+</h2>
-              <h1>Culture</h1>
-              <p>
-                Hip-hop, cultural movement that attained widespread popularity in the 1980s
-                and ’90s; also, the backing music for rap, the musical style incorporating
-                rhythmic and/or rhyming speech that became the movement’s most lasting and
-                influential art form.
-              </p>
-              <button>Get Started</button>
+    <div className="Container">
+        <div className="Wrapper">
+              <div className="Info">
+                  <div className="CenterInfo">
+                      <h2>Welcome to</h2>
+                      <h2>introducción</h2>
+                      <h1>HIPHOP</h1>
+                      <h2>+</h2>
+                      <h1>Culture</h1>
+                      <p>
+                          Hip-hop, cultural movement that attained widespread popularity in the 1980s
+                          and ’90s; also, the backing music for rap, the musical style incorporating
+                          rhythmic and/or rhyming speech that became the movement’s most lasting and
+                          influential art form.                        
+                      </p>
+                      <button>Animación Inversa</button>
+                  </div>
+              </div>
+              <div className="Imagenes">
+                  <div className="Colum1">
+                      <img src={RapOne} alt="Rapper" />
+                  </div>
+                  <div className="Colum2">
+                      <img src={RapTwo} alt="Stage" />
+                  </div>
+              </div>
           </div>
-        </div>
-        <div className="Images">
-          <div className="Colum1">
-            <img src={RapOne} alt="rapper"/>
-          </div>
-          <div className="Colum2">
-            <img src={RapTwo} alt="stage"/>
-          </div>
-        </div>
       </div>
-    </div>
+    
   );
 }
 
